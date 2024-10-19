b6ea7ae1a0c8f743131102ea1ac108415ddffd7b
Inocencio Cordeiro <cordeiroturorias@gmail.com>
2024-10-19 22:49:31 +0100

    created AIservice

--- src/main/java/com/censoia/angola/service/AiService.java
+++ src/main/java/com/censoia/angola/service/AiService.java
@@ -1,4 +1,9 @@
 package com.censoia.angola.service;
 
 public class AiService {
+    private static final String API_URL = "https://api.openai.com/v1/chat/completions";
+    private static final String API_KEY = "";
+
+
+
 }
